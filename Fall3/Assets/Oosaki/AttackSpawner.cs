a3952b35382c4bb3e551facdf6b1ddbddbf199c6
tw4706 <towajobseach@gmail.com>
2026-02-15 12:34:55 +0900

    コリジョンマネージャー追加

---
+++ Fall3/Assets/Oosaki/AttackSpawner.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackSpawner: MonoBehaviour
+{
+    //生成する球
+    [SerializeField] GameObject ballPrefab;
+    //出現位置
+    [SerializeField] Transform spawnPoint;
+
+    //今出てる弾
+    GameObject _currentBall; 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // 弾が無い時だけ発射
+            if (_currentBall == null)
+            {
+                SpawnBall();
+            }
+        }
+    }
+
+    void SpawnBall()
+    {
+        // 球を生成
+        GameObject ball = Instantiate(
+            ballPrefab,
+            spawnPoint.position,
+            Quaternion.identity
+        );
+
+        // 前方向に飛ばす（仮）
+        Rigidbody rb = ball.GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * 300f);
+    }
+}
