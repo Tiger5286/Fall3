488b4b907b2675ae008b1b702df574b0b701da69
KaminagaAyumu <kaminagajobsearch@gmail.com>
2026-02-16 03:23:07 +0900

    プレイヤーの参加を管理するマネージャーを追加

---
+++ Fall3/Assets/Kaminaga/JoinManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(PlayerInputManager))]
+public class JoinManager : MonoBehaviour
+{
+    private PlayerInputManager _playerInputManager;
+
+    private void Awake()
+    {
+        _playerInputManager = GetComponent<PlayerInputManager>();
+    }
+
+    private void OnEnable()
+    {
+        Debug.Log("OnEnable");
+
+        _playerInputManager.playerJoinedEvent.AddListener(OnPlayerJoinedUnityEvent);
+        _playerInputManager.playerLeftEvent.AddListener(OnPlayerLeftUnityEvent);
+    }
+
+    private void OnDisable()
+    {
+        if (_playerInputManager == null)
+        {
+            return;
+        }
+
+        _playerInputManager.playerJoinedEvent.RemoveListener(OnPlayerJoinedUnityEvent);
+        _playerInputManager.playerLeftEvent.RemoveListener(OnPlayerLeftUnityEvent);
+    }
+
+    private void OnPlayerJoinedUnityEvent(PlayerInput p) => HandleJoined(p, "UnityEvent");
+
+    private void OnPlayerLeftUnityEvent(PlayerInput p) => Debug.Log($"[JoinManager] Left(UnityEvent): idx={p.playerIndex}");
+
+    private void HandleJoined(PlayerInput player, string via)
+    {
+        Debug.Log($"[JoinManager] Joined({via}): idx={player.playerIndex}, device = {string.Join(",", player.devices)}");
+
+        if (player.currentActionMap == null || player.currentActionMap.name != "Gameplay")
+        {
+            player.SwitchCurrentActionMap("Gameplay");
+        }
+        //InputManager.Instance.RegisterPlayer(player);
+    }
+}
