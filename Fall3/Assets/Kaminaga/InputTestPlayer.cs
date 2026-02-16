70e4759a148f60fa1fcd4d67f9e1281f5b17311d
KaminagaAyumu <kaminagajobsearch@gmail.com>
2026-02-16 13:46:53 +0900

    インプットマネージャーを追加

---
+++ Fall3/Assets/Kaminaga/InputTestPlayer.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+// プレイヤーインプットを取得した上で使う
+[RequireComponent(typeof(PlayerInput))]
+public class InputTestPlayer : MonoBehaviour
+{
+    public Vector2 _move;
+    public float _moveSpeed = 5.0f;
+    public float _jumpForce = 1.5f;
+    public int _playerIndex;
+
+    public void OnEnable()
+    {
+        if(InputManager.Instance != null)
+        {
+            InputManager.Instance.OnMoveInput += HandleMoveInput;
+            InputManager.Instance.OnJumpInput += HandleJumpInput;
+        }
+    }
+
+    public void OnDisable()
+    {
+        if(InputManager.Instance != null)
+        {
+            InputManager.Instance.OnMoveInput -= HandleMoveInput;
+            InputManager.Instance.OnJumpInput -= HandleJumpInput;
+        }
+    }
+
+    public void HandleMoveInput(int idx, Vector2 moveValue)
+    {
+        if(idx != _playerIndex) return;
+
+        _move = moveValue;
+    }
+
+    public void HandleJumpInput(int idx)
+    {
+        if(idx != _playerIndex) return;
+        Debug.Log($"Player{idx} Jump!");
+    }
+
+    private void Awake()
+    {
+        _playerIndex = GetComponent<PlayerInput>().playerIndex;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
