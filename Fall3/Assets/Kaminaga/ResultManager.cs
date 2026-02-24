1613091d83cea71dd356d7dbf7a6841b0c745af4
KaminagaAyumu <kaminagajobsearch@gmail.com>
2026-02-24 16:53:59 +0900

    リザルトのシーン遷移とUI管理クラスを追加

--- Fall3/Assets/Kaminaga/ResultManager.cs
+++ Fall3/Assets/Kaminaga/ResultManager.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ResultManager : GameManagerBase
 {
+    [Header("シーン管理")]
+    [SerializeField] private SceneManagerKaminaga _sceneManager;
 
     private void OnEnable()
     {
         Debug.Log("Result開始");
+        // プレイヤーの操作状態を非アクティブにする
+        InputManager.Instance.SetAllPlayerControl(false);
     }
 
     private void OnDisable()
     {
         Debug.Log("Result終了");
+    }
+
+    public void OnRetry()
+    {
+        _sceneManager.ChangeScene(SceneType.InGame);
+    }
+
+    public void OnReturnTitle()
+    {
+        _sceneManager.ChangeScene(SceneType.Title);
     }
 
     // Start is called before the first frame update
